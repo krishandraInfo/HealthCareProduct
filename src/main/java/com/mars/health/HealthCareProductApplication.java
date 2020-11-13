c305b2c17c593d960de424265450fa6b555659c8
krishna <javabykrishna@gmail.com>
2020-11-13 22:18:13 +0530

    Project Setup Commit

---
+++ src/main/java/com/mars/health/HealthCareProductApplication.java
@@ -0,0 +1,13 @@
+package com.mars.health;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCareProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthCareProductApplication.class, args);
+	}
+
+}
